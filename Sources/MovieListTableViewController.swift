b74de922cfdc0fb2c18806024af22489c91db79f
reich452 <reich452@regis.edu>
2017-03-10 12:38:16 -0700

    added search bar delegate

--- Sources/MovieListTableViewController.swift
+++ Sources/MovieListTableViewController.swift
@@ -14,6 +14,7 @@ class MovieListTableViewController: UITableViewController, UISearchBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "SHOW TIME...search"
+        movieSearchBar.delegate = self 
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
